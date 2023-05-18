6ac79236c59cd57a2c8dbe945991c210a94af9b1
szklaudika <klaudika.sz362@gmail.com>
2023-05-18 11:26:27 +0200

    Update insert_product.php

--- insert_product.php
+++ insert_product.php
@@ -1,46 +1,51 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 global $conn;
-include('server\connection.php');
-include('server\get_featured_products.php');
-if(isset($_POST['insert_product'])){
-    $product_name = $_POST['product_name'];
-    $product_description = $_POST['product_description'];
-    $product_price = $_POST['product_price'];
-    $product_status='true';
+include_once 'admin_header.php';
+?>
+<?php
 
-    $product_image = $_FILES['product_image']['name'];
-    $product_image2 = $_FILES['product_image2']['name'];
-    $product_image3 = $_FILES['product_image3']['name'];
-    $product_image4 = $_FILES['product_image4']['name'];
+include ('server/connection.php');
 
-    $temp_image = $_FILES['product_image']['tmp_name'];
-    $temp_image2 = $_FILES['product_image2']['tmp_name'];
-    $temp_image3 = $_FILES['product_image3']['tmp_name'];
-    $temp_image4 = $_FILES['product_image4']['tmp_name'];
+if (isset($_POST['create_product'])){
 
-    if($product_name=='' or $product_description=='' or $product_price=='' or $product_image=='' or $product_image2=='' or $product_image3=='' or $product_image4=='') {
-        echo "<script>alert('Please fill all the available fields')</script>";
-        exit();
+    $product_name = $_POST['name'];
+    $product_description = $_POST['description'];
+    $product_price = $_POST['price'];
+    $product_special_offer = $_POST['offer'];
+    $product_category = $_POST['category'];
+
+    $image1 = $_FILES['image1']['tmp_name'];
+    $image2 = $_FILES['image2']['tmp_name'];
+    $image3 = $_FILES['image3']['tmp_name'];
+    $image4 = $_FILES['image4']['tmp_name'];
+    //$file_name = $_FILES['image1']['name'];
+
+    $image_name1 = $product_name."1.jpg";
+    $image_name2 = $product_name."2.jpg";
+    $image_name3 = $product_name."3.jpg";
+    $image_name4 = $product_name."4.jpg";
+
+    move_uploaded_file($image1,"images/".$image_name1);
+    move_uploaded_file($image2,"images/".$image_name2);
+    move_uploaded_file($image3,"images/".$image_name3);
+    move_uploaded_file($image4,"images/".$image_name4);
+
+    $stmt = $conn->prepare("INSERT INTO products (product_name, product_description, product_price, product_special_offer,
+                      product_image, product_image2, product_image3, product_image4) VALUES (?,?,?,?,?,?,?,?) ");
+    $stmt->bind_param('ssssssss', $product_name, $product_description,
+        $product_price, $product_special_offer, $image_name1, $image_name2, $image_name3, $image_name4);
+
+    if($stmt->execute()){
+        header('location: products.php?product_created=Product has been updated successfully :)');
+
     }else{
-        move_uploaded_file($temp_image,"images/$product_image");
-        move_uploaded_file($temp_image2,"images/$product_image2");
-        move_uploaded_file($temp_image3,"images/$product_image3");
-        move_uploaded_file($temp_image4,"images/$product_image4");
-
-        $insert_products="insert into `products` (product_name, product_description, product_price, product_image, product_image2, product_image3, product_image4, date, status)
-                          values ('$product_name','$product_description','$product_price','$product_image','$product_image2','$product_image3','$product_image4,',NOW(),'$product_status')";
-        $result_query=mysqli_query($conn, $insert_products);
-        if($result_query){
-            echo "<script>alert('Successfully inserted the products')</script>";
-        }
+        header('location: products.php?product_failed=Error occured, try again :(');
     }
 }
 
-?>
 
+
+?>
     <style>
         body {
 
@@ -50,9 +55,6 @@ if(isset($_POST['insert_product'])){
             /* background-repeat: no-repeat; */
         }
     </style>
-<?php
-include_once 'header.php';
-?>
 
 <div class="item-details-page">
     <div class="container">
@@ -63,72 +65,82 @@ include_once 'header.php';
                     <h2>Insert <em>product</em></h2>
                 </div>
             </div>
-
-            <div class="container">
-                <div class="row justify-content-center" action="" method="POST" enctype="multipart/form-data">
-                    <div class="col-md-6">
-                        <form class="d-flex flex-column align-items-center">
-                            <div class="form-group">
-                                <label for="product_name">Product Title</label>
-                                <input type="text" class="form-control" id="product_name" name="product_name" style="width: 460px;">
-                            </div>
-                            <br><br>
-                            <div class="form-group">
-                                <label for="product_description">Product Description</label>
-                                <textarea class="form-control" id="product_description" name="product_description" rows="3" style="width: 460px;"></textarea>
-                            </div>
-                            <br><br>
-                            <div class="form-group">
-                                <label for="product_image">Product Image 1</label>
-                                <input type="file" class="form-control-file" id="product_image" name="product_image" style="width: 460px;">
-                            </div>
-                            <br><br>
-                            <div class="form-group">
-                                <label for="product_image2">Product Image 2</label>
-                                <input type="file" class="form-control-file" id="product_image2" name="product_image2" style="width: 460px;">
-                            </div>
-                            <br><br>
-                            <div class="form-group">
-                                <label for="product_image3">Product Image 3</label>
-                                <input type="file" class="form-control-file" id="product_image3" name="product_image3" style="width: 460px;">
-                            </div>
-                            <br><br>
-                            <div class="form-group">
-                                <label for="product_image4">Product Image 4</label>
-                                <input type="file" class="form-control-file" id="product_image4" name="product_image4 " style="width: 460px;">
-                            </div>
-                            <br><br>
-                            <div class="form-group">
-                                <label for="product_price">Price</label>
-                                <input type="number" class="form-control" id="product_price" name="product_price" style="width: 460px;">
-                            </div>
-                            <br><br>
-                            <div class="form-group text-center">
-                                <button type="submit" name="insert_product" class="btn btn-primary">Submit</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-
-
-
         </div>
 
+        <div class="container">
+            <div class="row justify-content-center" action="" method="POST" enctype="multipart/form-data">
+                <p style="color: red"><?php if (isset($_GET['error'])){echo $_GET['error'];} ?></p>
+                <div class="col-md-6">
+                    <form class="d-flex flex-column align-items-center" id="create-form" enctype="multipart/form-data" method="POST" action="insert_product.php">
+                        <div class="form-group">
+                            <label for="product-name">Product Title</label>
+                            <br>
+                            <input type="text" class="form-control" id="product-name" name="name" style="width: 460px;">
+                        </div>
+                        <br><br>
+                        <div class="form-group">
+                            <label for="product-desc">Product Description</label>
+                            <br>
+                            <textarea class="form-control" id="product-desc" name="description" rows="3" style="width: 460px;"></textarea>
+                        </div>
+                        <br><br>
+                        <div class="form-group">
+                            <label for="product-price">Price</label>
+                            <br>
+                            <input type="text" class="form-control" id="product-price" name="price" style="width: 460px;">
+                        </div>
+                        <br><br>
+                        <div class="form-group">
+                            <label for="product-offer">Special Offer/Sale</label>
+                            <br>
+                            <input type="number" class="form-control" id="product-offer" name="offer" style="width: 460px;">
+                        </div>
+                        <br><br>
+                        <div class="form-group">
+                            <label for="category">Category</label>
+                            <br>
+                            <label>
+                                <select class="form-select" required name="category">
+                                    <option value="originals">Originals</option>
+                                    <option value="prints">Prints</option>
+                                    <option value="commissions">Commissions</option>
+                                </select>
+                            </label>
+                        </div>
+                        <br><br>
+                        <div class="form-group">
+                            <label for="image1">Product Image 1</label>
+                            <br>
+                            <input type="file" class="form-control-file" id="image1" name="image1" style="width: 460px;">
+                        </div>
+                        <br><br>
+                        <div class="form-group">
+                            <label for="image2">Product Image 2</label>
+                            <br>
+                            <input type="file" class="form-control-file" id="image2" name="image2" style="width: 460px;">
+                        </div>
+                        <br><br>
+                        <div class="form-group">
+                            <label for="image3">Product Image 3</label>
+                            <br>
+                            <input type="file" class="form-control-file" id="image3" name="image3" style="width: 460px;">
+                        </div>
+                        <br><br>
+                        <div class="form-group">
+                            <label for="image4">Product Image 4</label>
+                            <br>
+                            <input type="file" class="form-control-file" id="image4" name="image4" style="width: 460px;">
+                        </div>
+                        <br><br>
+                        <div class="form-group text-center">
+                            <button type="submit" name="create_product" class="btn btn-primary">Create</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-
-
 </div>
-
-
-
-<br><br><br><br><br><br><br>
-<!--Section: Contact v.2-->
-
-
-
-
-
 <?php
 include_once 'footer.php';
 ?>
